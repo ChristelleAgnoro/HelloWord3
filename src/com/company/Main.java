03c5aef721240949df6e977830a0d557cfd9288c
christelleagnoro <christelle.agnoro@bison.howard.edu>
2022-02-02 16:21:47 -0500

    copie code over.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
+        // Declaring keyboard as a scanner to get user input
+        Scanner keyboard = new Scanner(System.in);
+
+        // Prompt ask for name
+        System.out.print ("What is your name? ");
+
+        // User answer
+        String input = keyboard.nextLine();
+
+        // Hello message to user
+        System.out.println( "Hello "+ input + " !" );    }
 }
